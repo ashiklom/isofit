75601e089c1b25fc414b1435141577e43a47d818
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2020-07-20 16:53:28 -0400

    Working minimal Python hypertrace interface

--- examples/py-hypertrace/workflow.py
+++ examples/py-hypertrace/workflow.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 
-import pathlib
+from pathlib import Path
+from atexit import register
 import json
 import ray
+import spectral
+import pickle
 
 import numpy as np
 
@@ -17,22 +20,38 @@ from isofit.core.geometry import Geometry
 
 with open("./config.json") as f:
     config = json.load(f)
-wavelength_file = pathlib.Path(config["wavelength_file"]).resolve()
-surface_file = pathlib.Path(config["surface_file"]).resolve()
+
+wavelength_file = Path(config["wavelength_file"]).expanduser().resolve()
+surface_file = Path(config["surface_file"]).expanduser().resolve()
+reflectance_file = Path(config["reflectance_file"]).expanduser().resolve()
 forward_settings = config["isofit"]["forward_model"]
 forward_settings["instrument"]["wavelength_file"] = wavelength_file
 surface_settings = forward_settings["surface"]
 surface_settings["surface_file"] = surface_file
 surface_settings["wavelength_file"] = wavelength_file
 
+# Make RTM paths absolute
+vswir_settings = config["isofit"]["forward_model"]["radiative_transfer"]["radiative_transfer_engines"]["vswir"]
+for key in ["lut_path", "template_file", "engine_base_dir"]:
+    vswir_settings[key] = Path(vswir_settings[key]).expanduser().resolve()
+
 if not ray.is_initialized():
     ray.init()
+    register(ray.shutdown)
 fm = ForwardModel(Config({"forward_model": forward_settings}))
 
 inverse_settings = config["isofit"]["implementation"]
 inverse_settings["mode"] = "inversion"
 
-iv = Inversion(inverse_config, fm)
+geomvec = [
+    -999, # path length; not used
+     0, # Observer azimuth; Degrees 0-360; 0 = Sensor in N, looking S; 90 = Sensor in W, looking E
+     0, # Observer zenith; Degrees 0-90; 0 = directly overhead, 90 = horizon
+     0, # Solar azimuth; Degrees 0-360; 0 = N, 90 = W, 180 = S, 270 = E
+     0   # Solar zenith; not used (determined from time)
+ ]
+
+iv = Inversion(Config({"implementation": inverse_settings}), fm)
 igeom = Geometry(obs=geomvec)
 
 def htworkflow(refl, aot, h2o, fm, iv, igeom):
@@ -44,6 +63,12 @@ def htworkflow(refl, aot, h2o, fm, iv, igeom):
     return radiance, state_trajectory, unc
 
 htworkflow_r = ray.remote(htworkflow)
-results = ray.get([htworkflow_r.remote(refl, *args) for refl in reflectance])
 
-import gdal
+reflectance_img = spectral.open_image(reflectance_file)
+reflectance = spectral.algorithms.iterator(reflectance_img)
+
+for aot in config["hypertrace"]["true_AOD"]:
+    for h2o in config["hypertrace"]["true_H2O"]:
+        results = ray.get([htworkflow_r.remote(refl, aot, h2o, fm, iv, igeom) for refl in reflectance])
+
+pickle.dump(results, open("results.pkl", "wb"))
